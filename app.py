38479439c2ca1161bf44d82097b516a9946fdbb4
YAMANOE <139040160+YAMANOE@users.noreply.github.com>
2023-09-18 00:19:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -17,45 +17,33 @@ class NewAccount:
     def __str__(self):
         return f"your name is {self.name},your email is {self.email}"
     
-carriers=["Data scientist","Software developer","Loading and unloading corge","accounting and finance","Arrange the goods","electrician","sanitation worker"]   
-chronic_diseases=['Heart disease','Cancer','Stroke','Diabetes','Kidney disease']
-class Jobs(NewAccount)  : 
-    def __init__(self,name,email,password,age,adress,height,weight,carrier,phonenumber,chronicdiseases):
-        super().__init__(name,email,password)
+carriers=[" Data scientist"," Software developer"," Loading and unloading corge"," accounting and finance"," Arrange the goods"," electrician","sanitation worker"]   
+chronic_diseases=['1- Heart disease','2- Cancer','3- Stroke','4- Diabetes','5- Kidney disease']
+class Jobs()  : 
+    def __init__(self,name,email,age,adress,height,weight,carrier,phonenumber,chronicdiseases):
+        self.name=name
+        self.email=email
         self.age=int(age)
         self.adress=adress
         self.height=height
         self.weight=float(weight)
         self.carrier=carrier
         self.phonenumber=phonenumber
-        self.password=password
         self.chronicdiseases=chronicdiseases
-        if self.weight <40 or self.weight >120:
+        if self.weight <40 or self.weight >140:
             print('Sorry, you do not meet the conditions')
         if self.age>60 :
-            print('Sorry, you do not meet the conditions') 
-        for i in carriers :
-            if self.carrier==i  :
-                print('You have fulfilled the first requirements. ')    
-            else:
-                print("We are sorry, the position you are looking for is currently unoccupied")
-            break
-        if self.chronicdiseases=='yse':
-            print("We are sorry")
-        else:
-             print('We look forward to seeing you soon')  
-
+            print('Sorry, you do not meet the conditions')  
     def __str__(self):
         return f' name : {self.name}, age : {self.age}, email : {self.email}, adress : {self.adress}, height is : {self.height} , weight :{self.weight}, careier :{self.carrier} , phone number : {self.phonenumber},chronic diseases: {self.chronicdiseases}'        
 
-class Employees(Jobs):
-    def __init__(self,name,email,password,age,adress,height,weight,carrier,phonenumber,chronicdiseases,sectionwork,fulltime,parttime):
-        super().__init__(name,email,password,age,adress,height,weight,carrier,phonenumber,chronicdiseases)
+class Employees():
+    def __init__(self,sectionwork,fulltime,parttime):
         self.sectionwork=sectionwork
         self.fulltime=fulltime
         self.parttime=parttime    
     def __str__(self):
-        return f'{self.__str__()}, sectionwork : {self.sectionwork}, full time : {self.fulltime},part time : {self.parttime }'
+        return f' sectionwork : {self.sectionwork}, full time : {self.fulltime},part time : {self.parttime }'
      
 Investmentcorporate={'name':"",'email':""}
 lowercost=['5m','6m','7m','8m','9m']
@@ -73,7 +61,7 @@ class Investment :
         if self.cost<=90000:
             print('Spaces available for this investment : ')
             print(lowercost)
-        if self.cost>100000:
+        if self.cost>=100000:
             print('Spaces available for this investment : ')
             print(largestcost) 
         print('Half the amount is paid every six months . ')  
@@ -90,19 +78,39 @@ class PurchaseGoods :
         if self.productweight < 30000:
             print('Sorry, the quantity is small')   
         else:
-            print('We will contact you soon')    
+            print('We will contact you soon')   
+# class              
 
 #<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< main code :  >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+c=1
+v=2
+
+total=0
 print("Do you have an account with us?")
-choose=input("please choose yes or no ")
+choose=input("please choose yes or no  : ")
 newaccounts={"name":"","email":"","password":""}
-employees={"name":"",'email':"","age":"",'password':'','adress':"",'height':"","weight":"","carrier":"","phonenumber":"","chronicdiseases":"","sectionwork":"","fulltime":"","parttime":""}
+employees={"name":"",'email':"","age":"",'adress':"",'height':"","weight":"","carrier":"","phonenumber":"","chronicdiseases":"","sectionwork":"","fulltime":"","parttime":""}
 if choose=="no":
     name=input("enter your name : ")
-    email=input("enter your email : ")
+    while c<v:
+        email=input("enter your email : ")
+        for i in range(len(email)):
+            if email[i]!='@':
+                print('wrong input .')
+                c+=1
+                v+=1
+        break                       
+    print('Your password must consist of at least eight characters . ')    
     password=input('enter your password : ')
-    newacc=NewAccount(name,email,password)  
-    print("<<<<<<  complate account  >>>>>>") 
+    for i in range(len(password)):
+        total+=1
+        if total<8:
+            print('wrong input .')
+            break
+        newacc=NewAccount(name,email,password)  
+        print("<<<<<<  complate account  >>>>>>") 
     for i in range(10):     
         print("What services do you want?")
         print('1-Job search')
@@ -110,15 +118,25 @@ if choose=="no":
         print('3-Purchase Goods')
         print('4-employee')
         schoos2=int(input('Type the service number you want : '))
-        if schoos2==1 or schoos2==4:
+        if schoos2==1 :
+             name=input("enter your name : ")
+             email=input("enter your email : ")
              age=input('enter your age : ')
              adress=input('enter your adress : ')
              height=input('enter your height  : ')
              weight=input('enter your weight : ')
-             carrier=input('enter your carrier :')
+             print(carriers) 
+             carrier=input('Write your specialty as it appears in front of you : ')
              phonenumber=input('enter your phonenumber : ')
              chronicdiseases=input('Do you have any serious chronic diseases? Answer yes or no :')
-             staff1=Jobs(name,email,password,age,adress,height,weight,carrier,phonenumber,chronicdiseases)
+             if chronicdiseases=='yes' or chronicdiseases=='Yes':
+                 print(chronic_diseases)
+                 choose5=input('Do you have any of these diseases?,enter yes or no : ')
+                 if choose5=='yes':
+                     print('We are sorry')
+                     continue    
+             print('We look forward to seeing you soon .')            
+             staff1=Jobs(name,email,age,adress,height,weight,carrier,phonenumber,chronicdiseases)
              print(staff1)
              for i in employees:
                 if i=='name':
